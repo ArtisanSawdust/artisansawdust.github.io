f4ba11f5489cb3b47fe93dc1e586c167959ccc0a
Morgan Prior <36314+morganp@users.noreply.github.com>
2024-10-10 17:21:14 +0100

    added first post

---
+++ create_new_post.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# coding=utf-8
+
+import html
+# CLI for creating a new markdown post
+
+
+from datetime import datetime
+
+# datetime object containing current date and time
+now = datetime.now()
+ 
+# dd/mm/YY H:M
+dt_string = now.strftime("%Y-%m-%d %H:%M")
+dt_string_simple = now.strftime("%Y-%m-%d")
+
+post_title = input("Enter Post Title : ")
+
+post_title = html.escape(post_title) # sanitize
+post_title_safe = post_title.replace(' ', '_')
+
+new_post_name = dt_string_simple + '_' + post_title_safe
+
+this_file_name = "./content/posts/"+new_post_name+".md"
+
+with open(this_file_name, "w") as file:
+    file.write("Title: " + post_title + "\n")
+    file.write("Date: %s \n" % dt_string)
+    file.write("Category: Networking\n")
+    file.write("Tags: network, isp\n")
+    file.write("Author: morganp\n")
+
+print(this_file_name)
